61b0f1d5d6a9c30104dab07eef793f1f5980fcfd
Jailyss Neust <kabakovmaksim76@gmail.com>
2022-11-29 05:56:22 +0300

    pep

--- generator/views.py
+++ generator/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import random
 
-# Create your views here.
+
 def home(request):
     return render(request, 'generator/home.html')
+
 
 def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
@@ -20,7 +21,4 @@ def password(request):
     for i in range(length):
         thepassword += random.choice(characters)
     return render(request, 'generator/password.html', {'password': thepassword})
-
-
-
 
